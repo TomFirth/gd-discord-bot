4ba27df91d60c1a951811be9e561886d7274449d
Tom Firth <tom.firth84@gmail.com>
2026-07-14 09:08:58 +0100

    remove test

--- src/commands/index.js
+++ src/commands/index.js
@@ -1,9 +1,7 @@
 import { compareGames } from './steam.js';
-import { testTheme } from './themeTest.js';
 import gamedevCommand from './idea.js';
 
 export const commandHandlers = {
   compareGames,
-  testTheme,
   gamedev: gamedevCommand.execute,
 };
