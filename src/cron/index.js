19c3b83c83c282b04cf2c15d085f0d7eec0ddd5f
Tom Firth <tom.firth84@gmail.com>
2026-07-22 19:51:00 +0100

    prompts

--- src/cron/index.js
+++ src/cron/index.js
@@ -1,6 +1,6 @@
 import { theme } from './theme.js';
 import { startRedditFeeds } from './reddit.js';
-import { initializePromptSchedules } from './prompts.js';
+import { initializePromptSchedules } from '../services/prompts.js';
 
 export const initializeScheduledEvents = (client) => {
   theme(client);
